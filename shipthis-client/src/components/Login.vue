83c522b2349c9cea5bda47a9b0c4ce82bbd4bd0c
david-monroy <63082936+david-monroy@users.noreply.github.com>
2020-05-17 12:57:34 -0400

    FASE1-012: Recuperar contraseña [Front] (#10) :art:
    * :sparkles: Recuperar contraseña
    
    Se implemento el formulario para recuperar la contraseña
    
    * :zap: Recuperar contraseña
    
    Correccion en la informacion solicitada
    
    * :pencil: Documentacion incluida
    
    Inclusion de codigo documentado correspondiente

--- shipthis-client/src/components/Login.vue
+++ shipthis-client/src/components/Login.vue
@@ -1,9 +1,11 @@
 <template>
+  <!-- Modal para el inicio de sesión -->
   <v-row id="LoginForm" justify="center" align="center" class="ma-0 pa-0">
     <v-col cols="12" class="ma-0 pa-0">
       <v-card class="ma-0">
-              <v-window  class="ma-0">
-                <v-window-item  class="ma-0">
+              <v-window  class="ma-0" v-model="step">
+                <!-- Slide 1: Inicio de sesión -->
+                <v-window-item  class="ma-0" :value="1">
                   <v-row class="ma-0">
                     <v-col cols="8" class="ma-0">
                       <v-card-text class="mt-3">
@@ -17,21 +19,22 @@
                         </div>
                         <h4 class="text-center mt-4">{{ message }}</h4>
                         <v-form @submit.prevent="submit">
-                          <v-text-field label="User" name="User" type="text" 
-                          prepend-icon="person" color="primary accent-3" v-model.trim="$v.user.$model"
-                          :class="{ 'is-invalid': $v.user.$error, 'is-valid': $v.user.$invalid } " />
-                          <!-- <div v-if="!$v.user.required">Field is required</div> -->
+                          <v-text-field label="Correo electrónico" name="Email" type="text" 
+                          prepend-icon="email" color="primary accent-3" v-model.trim="$v.email.$model"
+                          :class="{ 'is-invalid': $v.email.$error, 'is-valid': $v.email.$invalid } " />
                           <v-text-field id="Password" label="Password" name="Password" type="password" 
                           prepend-icon="lock" color="primary accent-3" />
                         
-                        <h3 class="text-center mt-3 secondary--text">{{ forgotMyPassword }}</h3>
-                      
+                        
                       <div class="text-center my-3">
                         <v-btn rounded type="submit" :disabled="submitStatus === 'PENDING'" color="success accent-3 accent--text" dark>{{ loginBtn }}</v-btn>
                       </div>
                       <p class="text-center" v-if="submitStatus === 'ERROR'">{{ submitMsg.error }}</p>
                       <p class="text-center" v-if="submitStatus === 'OK'">{{ submitMsg.ok }}</p>
                       <p class="text-center" v-if="submitStatus === 'PENDING'">{{ submitMsg.pending }}</p>
+
+                      <h3 class="text-center mt-3 secondary--text forgotPassword" @click="step++">{{ forgotMyPassword }}</h3>
+                      
                       </v-form>
                       </v-card-text>
                     </v-col>             
@@ -41,6 +44,43 @@
                         <v-btn rounded @click="goRegistry" color="success accent-3 accent--text" outlined="">{{ signinBtn }}</v-btn>
                       </div>
                     </v-col>
+                  </v-row>
+                </v-window-item>
+
+                <!-- Slide 2: Recuperar contraseña -->
+                <v-window-item  class="ma-0" :value="2">
+                  <v-row class="ma-0">          
+                    <v-col cols="4" md="4" class="primary accent-3 center-img" justify="center" align="center" >
+                      <img class="login-logo" src="../assets/home/logo.png" alt="" />
+                      <h3 class="success--text mt-4 mb-2" @click="step--">{{ noAccount }}</h3>
+                      <div class="text-center">
+                        <v-btn rounded @click="goRegistry" color="success accent-3 accent--text"  outlined="">{{ signinBtn }}</v-btn>
+                      </div>
+                    </v-col>
+                    <v-col cols="8" class="ma-0">
+                      <v-card-text class="mt-3">
+                        <h1 class="text-center display-5 accent--text text--accent-3">{{ recoverPassword }}</h1>
+                        <h4 class="text-center mt-4">{{ insertData }}</h4>
+                        <v-form @submit.prevent="submit">
+                          <v-text-field label="Email" name="Correo electrónico" type="text" 
+                          prepend-icon="email" color="primary accent-3" v-model.trim="$v.email.$model"
+                          :class="{ 'is-invalid': $v.email.$error, 'is-valid': $v.email.$invalid } " />
+                          <!-- <div v-if="!$v.email.required">Field is required</div> -->
+                          <v-text-field id="identification" label="Número de identidad" name="identification" type="password" 
+                          prepend-icon="person" color="primary accent-3" />
+                      
+                      <div class="text-center my-3">
+                        <v-btn rounded type="submit" :disabled="submitStatus === 'PENDING'" color="success accent-3 accent--text" dark>{{ submit }}</v-btn>
+                      </div>
+                      <p class="text-center" v-if="submitStatus === 'ERROR'">{{ submitMsg.error }}</p>
+                      <p class="text-center" v-if="submitStatus === 'OK'">{{ submitMsg.ok }}</p>
+                      <p class="text-center" v-if="submitStatus === 'PENDING'">{{ submitMsg.pending }}</p>
+
+                      <h3 class="text-center mt-3 secondary--text forgotPassword" @click="step++">{{ goBack }}</h3>
+                      
+                      </v-form>
+                      </v-card-text>
+                    </v-col>   
                   </v-row>
                 </v-window-item>
               </v-window>
@@ -57,12 +97,18 @@ export default {
         activeLogin: Boolean        
     },
     data: () => ({
-      user: '',
+      email: '',
       title: 'Ingresa a Shipthis',
       message: 'Usa tu e-mail para entrar',
       forgotMyPassword: 'Olvidé mi contraseña',
       loginBtn: 'Ingresa',
       signinBtn: 'Regístrate',
+      insertData: 'Introduce tus datos',
+      goBack: 'Regresar',
+      submit: 'Enviar',
+      noAccount: '¿No tienes cuenta?',
+      step: 1,
+      recoverPassword: 'Recuperar mi contraseña',
       submitMsg: {
         error: 'Por favor, rellena los campos',
         pending: 'Procesando la información...',
@@ -73,8 +119,11 @@ export default {
         { id: "2", network: "Google", code: "fab fa-google-plus-g"}
       ]
     }),
+    props: {
+      source: String
+    },
     validations: {
-      user: {
+      email: {
         required,
         minLength: minLength(5)
       }
