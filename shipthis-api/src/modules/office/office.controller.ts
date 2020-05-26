1666ed9b48a2bac3fd7b8050d392f3990a98add8
yeisson-venencia <63071952+yeisson-venencia@users.noreply.github.com>
2020-05-25 21:14:02 -0400

    FASE1-031: Oficinas disponibles [Back-End] (#21)
    * :pencil: Insert de oficinas
    
    Se redactaron los insert para cargar los datos iniciales de offinas de shipthis junto con sus ubicaciones respectivas
    
    * :art: Servicio para lectura de officinas
    
    Se creo el servicio que permite obtener todas las oficinas activas registradas. Ademas se agrego el logger al servicio.

--- shipthis-api/src/modules/office/office.controller.ts
+++ shipthis-api/src/modules/office/office.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { OfficeService } from './office.service';
 
 @Controller('office')
-export class OfficeController {}
+export class OfficeController {
+  constructor(private readonly _officeService: OfficeService) {}
+
+  @Get('allActive')
+  async getAllActiveOffices() {
+    return this._officeService.getAllActiveOffices();
+  }
+}
