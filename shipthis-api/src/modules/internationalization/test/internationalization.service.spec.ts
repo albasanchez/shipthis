2b5868eb72e9b91c0c4c21dd1583319cba8af148
Alba Sofía <assanchez.17@est.ucab.edu.ve>
2020-08-09 18:00:14 -0400

    FASE3-015: Pruebas unitarias del servicio - internationalization

--- shipthis-api/src/modules/internationalization/test/internationalization.service.spec.ts
+++ shipthis-api/src/modules/internationalization/test/internationalization.service.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InternationalizationService } from '../internationalization.service';
+import { internationalizationMockModuleMetadata } from './mocks/internationalization.mock';
+import axios from 'axios';
+import { TraductionMock } from './mocks/traduction.mock';
+import { POEditorConnectionException } from '../../../common/exceptions';
 
 describe('InternationalizationService', () => {
   let service: InternationalizationService;
+  const traductionMock: TraductionMock = new TraductionMock();
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [InternationalizationService],
-    }).compile();
+    const module: TestingModule = await Test.createTestingModule(
+      internationalizationMockModuleMetadata,
+    ).compile();
 
     service = module.get<InternationalizationService>(
       InternationalizationService,
@@ -16,5 +21,34 @@ describe('InternationalizationService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should get dictionary', () => {
+    axios.post = traductionMock.post(true);
+    axios.get = traductionMock.get(true);
+    service.getDictionary('EN');
+    expect(axios.post).toHaveBeenCalled();
+  });
+
+  describe('dictionary exceptions', () => {
+    it('post should throw poeditor connection exception', async () => {
+      axios.post = traductionMock.post(false);
+      try {
+        await service.getDictionary('EN');
+      } catch (e) {
+        expect(axios.post).toHaveBeenCalled();
+        expect(e).toBeInstanceOf(POEditorConnectionException);
+      }
+    });
+    it('get should throw poeditor connection exception', async () => {
+      axios.post = traductionMock.post(true);
+      axios.get = traductionMock.get(false);
+      try {
+        await service.getDictionary('EN');
+      } catch (e) {
+        expect(axios.post).toHaveBeenCalled();
+        expect(e).toBeInstanceOf(POEditorConnectionException);
+      }
+    });
   });
 });
